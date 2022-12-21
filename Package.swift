d6f832cafedec91ccb61327664ae108ec3ae5b66
Cole Moore <Cartisim@icloud.com>
2022-12-21 12:15:56 +0300

    fixed Package manifest

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
         .library(
-            name: "LASDKiOS"
+            name: "LASDKiOS",
             type: .static,
             targets: ["SwiftLASDKiOS"]),
     ],
