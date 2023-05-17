6006f19b0d857a855ad13cae3e2b2bc2fde87b85
Cartisim Development <33076701+Cartisim@users.noreply.github.com>
2023-05-17 18:10:19 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
             ]
         ),
         .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0.xcframework.zip", checksum: "8b54aa6885c116a2e12bda636af8b63a6a815bbddb1d19aeda07080a7a07a0ec"),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.2.xcframework.zip", checksum: "a30b0df0221f8355b26f3f5a73acd059dde0dd1309b7036b1f928f26fe246cf6"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.2.xcframework.zip", checksum: "58f164fcd070715e04d3da5211369eda116e03c508a7136469b618caab0343f9"),
         .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m110-1.0.0.xcframework.zip", checksum: "a2f4cee24ce4389aa00feb86edd8dc8c67a43aedf1f8b4ceb5998c94f16a5e3d")
     ]
 )
