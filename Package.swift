3674e847a39f62a1840870e5fbac5744c955aac8
Cole Moore <Cartisim@icloud.com>
2023-05-30 23:32:34 +0400

    Updated Checksum

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
                 "CBARealTime"
             ]
         ),
-        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0.xcframework.zip", checksum: "163c724f58cabef4d8be61aa7dfc835f54931f0a522db66a6006b849b249265b"),
+        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0.xcframework.zip", checksum: "fb4cd01ebb41106acaecf469878c048a8a76dd506f34c0ec880004594b780153"),
         .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.2.xcframework.zip", checksum: "58f164fcd070715e04d3da5211369eda116e03c508a7136469b618caab0343f9"),
         .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m110-1.0.0.xcframework.zip", checksum: "a2f4cee24ce4389aa00feb86edd8dc8c67a43aedf1f8b4ceb5998c94f16a5e3d")
     ]
