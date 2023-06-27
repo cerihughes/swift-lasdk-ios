7c8af1af17a1a928e13c22fc46099cf7968710a8
Cole Moore <Cartisim@icloud.com>
2023-06-27 19:24:49 +0800

    Updated Checksums

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
                 "CBARealTime"
             ]
         ),
-        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0.xcframework.zip", checksum: "fb4cd01ebb41106acaecf469878c048a8a76dd506f34c0ec880004594b780153"),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.2.xcframework.zip", checksum: "58f164fcd070715e04d3da5211369eda116e03c508a7136469b618caab0343f9"),
-        .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m110-1.0.0.xcframework.zip", checksum: "a2f4cee24ce4389aa00feb86edd8dc8c67a43aedf1f8b4ceb5998c94f16a5e3d")
+        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0.xcframework.zip", checksum: "469809d6d68b7b854529ec09e24b46b0d3d2405b28d66654ac155a7d6594efe8"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.4.xcframework.zip", checksum: "1b32b01e266f4bd98aa6849567b4341a41905a40a958fd6c2061eebb8e8df386"),
+        .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
