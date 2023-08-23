12419936d426d11e3021008741c0d73a0256b340
Cole Moore <Cartisim@icloud.com>
2023-08-23 14:41:57 +0400

    Updated checksum for 2.0.0 beta 1 release

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
                 "WebRTC"
             ]
         ),
-        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0.xcframework.zip", checksum: "469809d6d68b7b854529ec09e24b46b0d3d2405b28d66654ac155a7d6594efe8"),
-        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.4.xcframework.zip", checksum: "1b32b01e266f4bd98aa6849567b4341a41905a40a958fd6c2061eebb8e8df386"),
+        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0.xcframework.zip", checksum: "3839c358dbe0fdfd536affec2daf8cf8172cc5bcd2f1fabfebe61e4cf0d2a05e"),
+        .binaryTarget(name: "FCSDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/FCSDKiOS-4.2.5.xcframework.zip", checksum: "432292a30184e01b05c942a478c154aa3d55e98e3401986ad3d93f250ed55fe3"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
