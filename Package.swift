48412091879d5717a99393f322c97fcaad307aed
cerihughes <hughesceri+github@gmail.com>
2023-08-25 09:18:32 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
                 "WebRTC"
             ]
         ),
-        .binaryTarget(name: "LASDKiOS", url: "https://https://www.dropbox.com/scl/fi/9jka6jmi2ncsfwij16e82/LASDKiOS-2.0.1.xcframework.zip?rlkey=ikn0quh9y00x7dtbpfab57z27&dl=1", checksum: "1daae15b6be75e289fde61120aed10c512dfd15577f5db2da5a9df2c583f8dfa"),
+        .binaryTarget(name: "LASDKiOS", url: "https://www.dropbox.com/scl/fi/9jka6jmi2ncsfwij16e82/LASDKiOS-2.0.1.xcframework.zip?rlkey=ikn0quh9y00x7dtbpfab57z27&dl=1", checksum: "1daae15b6be75e289fde61120aed10c512dfd15577f5db2da5a9df2c583f8dfa"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
